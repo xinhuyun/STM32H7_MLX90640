435b767e9bc3eb6464a372d49e255bb751c75cb0
xinhuyun <799637741@qq.com>
2026-01-19 14:53:53 +0800

    Initial commit

---
+++ MDK-ARM/RTE/_STM32H7_MLX90640/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32H7_MLX90640' 
+ * Target:  'STM32H7_MLX90640' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ MDK-ARM/STM32H7_MLX90640/startup_stm32h743xx.d
@@ -0,0 +1,1 @@
+stm32h7_mlx90640\startup_stm32h743xx.o: startup_stm32h743xx.s
